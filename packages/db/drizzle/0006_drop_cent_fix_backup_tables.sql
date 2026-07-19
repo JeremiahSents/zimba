a2d0eee1dde99ee6abdc2e8e201b35817e6c156d
Jeremiah <sentomerojeremy@gmail.com>
2026-07-19 19:05:21 +0300

    refactor: drop cent fix backup tables from database
    - Remove _cent_fix_backup_allocation table
    - Remove _cent_fix_backup_expense_line table
    - Remove _cent_fix_backup_ledger_payment table
    - Remove _cent_fix_backup_payable table

---
+++ packages/db/drizzle/0006_drop_cent_fix_backup_tables.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS "_cent_fix_backup_allocation";
+--> statement-breakpoint
+DROP TABLE IF EXISTS "_cent_fix_backup_expense_line";
+--> statement-breakpoint
+DROP TABLE IF EXISTS "_cent_fix_backup_ledger_payment";
+--> statement-breakpoint
+DROP TABLE IF EXISTS "_cent_fix_backup_payable";
